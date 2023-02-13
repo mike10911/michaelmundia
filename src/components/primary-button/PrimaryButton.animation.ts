c8e3fee9307780117f3641245a3c64d65c492740
mike10911 <mikeymundia@gmail.com>
2023-02-13 15:31:32 -0500

    button animation fix

--- src/components/primary-button/PrimaryButton.animation.ts
+++ src/components/primary-button/PrimaryButton.animation.ts
@@ -2,7 +2,7 @@ export const buttonAnimations = {
     hover: {
         scale: 1.1,
         transition: {
-            type: "spring", stiffness: 400, damping: 10, duration: 0.5
+            type: "spring", stiffness: 200, damping: 20, duration: 0.1
         }
     },
     tap: {
