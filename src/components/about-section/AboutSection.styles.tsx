7db0bdcf77fc78034ef8f41380e5ad212c2784af
mike10911 <mikeymundia@gmail.com>
2023-03-16 23:22:25 -0400

    about-content

--- src/components/about-section/AboutSection.styles.tsx
+++ src/components/about-section/AboutSection.styles.tsx
@@ -1,52 +1,104 @@
+import { motion } from "framer-motion";
 import styled from "styled-components";
 import { colors } from "../../lib/colors";
-import { H1, H2 } from "../../lib/typography";
-
+import { H1, H2, P } from "../../lib/typography";
 
 const StyledAboutSectionContainer = styled.div`
-    background-color: ${colors.LIGHT_GREEN};
-    margin-left: 5%;
-    margin-right: 5%;
-    height: 100em;
-    margin-top: 12em;
-    border-radius: 30px;
-    box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.05);
-`
+  background-color: ${colors.LIGHT_GREEN};
+  margin-left: 5%;
+  margin-right: 5%;
+  height: 120em;
+  margin-top: 12em;
+  border-radius: 30px;
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.05);
+`;
 const StyledAboutSectionTitle = styled(H1)`
-    text-align: center;
-    padding-top: 1.5em;
-    letter-spacing: 0.2rem;
-`
+  text-align: center;
+  padding-top: 1.5em;
+  letter-spacing: 0.2rem;
+`;
 const StyledTestimonialContainer1 = styled.div`
-    background-color: ${colors.WHITE};
-    border-radius: 60px;
-    width: 65%;
-    height: 25em;
-    margin-top: 62em;
-    margin-left: 3em;
-    box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.25);
-`
+  background-color: ${colors.WHITE};
+  border-radius: 60px;
+  width: 65%;
+  height: 25em;
+  margin-top: 50em;
+  margin-left: 3em;
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.25);
+`;
 
 const StyledTestimonialContainer2 = styled.div`
-    background-color: ${colors.WHITE};
-    border-radius: 60px;
-    width: 35%;
-    height: 45em;
-    margin-top: 42.3em;
-    margin-right: 3em;
-    box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.25);    
-`
+  background-color: ${colors.WHITE};
+  border-radius: 60px;
+  width: 35%;
+  height: 45em;
+  margin-top: 30.5em;
+  margin-right: 3em;
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.25);
+`;
 const StyledTestimonialsContainer = styled.div`
-    display: flex;
-    gap: 3rem;
-    `;
+  display: flex;
+  gap: 3rem;
+  
+`;
 const StyledTestimonialTitle = styled(H2)`
+  letter-spacing: 3.1rem;
+  margin-left: 3rem;
+  margin-top: -44.5rem;
+  font-size: 3.5rem;
+`;
+const AboutSectionContent = styled.div`
+    margin-left: 3em;
+    margin-right: 3em;
+    display: flex;
+    margin-top: 5em;
 
-    letter-spacing: 3.1rem;
-    margin-left: 3rem;
-    margin-top: -44.5rem;
-    font-size: 3.5rem;
+
+`;
+const StyledNameTextContainer = styled.div``;
+const StyledName = styled(H2)``;
+const StyledAboutText = styled(P)`
+    width: 90%;
+    margin-right: -5em;
+    max-height: 2em;
+    line-height: 1.5;
+`;
+
+const StyledImageSocialsContainer = styled.div``;
+
+const StyledAboutImage = styled.img`
+    scale: 0.8;
+    margin-top: -2em;
+    margin-right: 1em;
+    
+
+`;
+const StyledSocialsContainer = styled.div`
    
-  `;
+    justify-content: space-evenly;
+    display: flex;
+    align-items: center;
+   
 
-export { StyledAboutSectionContainer, StyledAboutSectionTitle, StyledTestimonialContainer1, StyledTestimonialContainer2, StyledTestimonialsContainer, StyledTestimonialTitle};
+`;
+
+const StyledAboutSocialIcon = styled(motion.a)`
+  color: ${colors.PARCHMENT};
+`;
+
+export {
+  StyledAboutSectionContainer,
+  StyledAboutSectionTitle,
+  StyledTestimonialContainer1,
+  StyledTestimonialContainer2,
+  StyledTestimonialsContainer,
+  StyledTestimonialTitle,
+  AboutSectionContent,
+  StyledNameTextContainer,
+  StyledName,
+  StyledAboutText,
+  StyledImageSocialsContainer,
+  StyledAboutImage,
+  StyledSocialsContainer,
+  StyledAboutSocialIcon,
+};
--- src/components/about-section/AboutSection.tsx
+++ src/components/about-section/AboutSection.tsx
@@ -1,24 +1,114 @@
 import React from "react";
 import {
+  AboutSectionContent,
+  StyledAboutImage,
   StyledAboutSectionContainer,
   StyledAboutSectionTitle,
+  StyledAboutSocialIcon,
+  StyledAboutText,
+  StyledImageSocialsContainer,
+  StyledName,
+  StyledNameTextContainer,
+  StyledSocialsContainer,
   StyledTestimonialContainer1,
   StyledTestimonialContainer2,
   StyledTestimonialsContainer,
   StyledTestimonialTitle,
 } from "./AboutSection.styles";
 
+import profile from "../../images/profile.png";
+import { BsLinkedin, BsGithub, BsFillCameraFill } from "react-icons/bs";
+import { MdMail } from "react-icons/md";
+import { buttonAnimations } from "../nav-bar/NavBar.animations";
+
 const AboutSection: React.FC = () => {
   return (
     <StyledAboutSectionContainer id="about">
       <StyledAboutSectionTitle color="white">ABOUT</StyledAboutSectionTitle>
+      <AboutSectionContent>
+        <StyledNameTextContainer>
+          <StyledName color="white">HELLO, I'M MIKE</StyledName>
+          <StyledAboutText color="white">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor.
+            <br />
+            <br />
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor.
+            <br />
+            <br />
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor.
+          </StyledAboutText>
+        </StyledNameTextContainer>
+        <StyledImageSocialsContainer>
+          <StyledAboutImage src={profile}></StyledAboutImage>
+          <StyledSocialsContainer>
+            <StyledAboutSocialIcon
+              variants={buttonAnimations}
+              whileHover="hover"
+              whileTap="tap"
+              href="https://www.linkedin.com/in/michael-mundia/"
+              target="_blank"
+            >
+              <BsLinkedin size="3rem" />
+            </StyledAboutSocialIcon>
+            
+            <StyledAboutSocialIcon
+              variants={buttonAnimations}
+              whileHover="hover"
+              whileTap="tap"
+              href="mailto:mikeymundia@gmail.com"
+              target="_blank"
+            >
+              <MdMail size="3rem" />
+            </StyledAboutSocialIcon>
+            <StyledAboutSocialIcon
+              variants={buttonAnimations}
+              whileHover="hover"
+              whileTap="tap"
+              href="https://github.com/mike10911"
+              target="_blank"
+            >
+              <BsGithub size="3rem" />
+            </StyledAboutSocialIcon>
+            <StyledAboutSocialIcon
+              variants={buttonAnimations}
+              whileHover="hover"
+              whileTap="tap"
+              href="https://mikemundia.wordpress.com/"
+              target="_blank"
+            >
+              <BsFillCameraFill size="3rem" />
+            </StyledAboutSocialIcon>
+          </StyledSocialsContainer>
+        </StyledImageSocialsContainer>
+      </AboutSectionContent>
       <StyledTestimonialsContainer>
         <StyledTestimonialContainer1></StyledTestimonialContainer1>
         <StyledTestimonialContainer2></StyledTestimonialContainer2>
       </StyledTestimonialsContainer>
       <StyledTestimonialTitle color="white">
-            TESTIMONIALS
-        </StyledTestimonialTitle>
+        TESTIMONIALS
+      </StyledTestimonialTitle>
     </StyledAboutSectionContainer>
   );
 };
