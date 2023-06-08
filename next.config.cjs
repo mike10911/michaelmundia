3d77e0c73cdfad2ee6caf7c78dff8680ab7dcaf0
mike10911 <mikeymundia@gmail.com>
2023-06-08 16:06:14 -0400

    next/image fix

--- next.config.cjs
+++ next.config.cjs
@@ -9,3 +9,15 @@ module.exports = withImages({
   inlineImageLimit: false
 })
 
+module.exports = {
+    webpack(config, options) {
+      config.module.rules.push({
+        test: /\.mp3$/,
+        use: {
+          loader: 'url-loader',
+        },
+      });
+      return config;
+    },
+  }; 
+
