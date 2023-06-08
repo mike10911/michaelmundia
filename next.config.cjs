e189a99f5085c447e517db32049433b7184ead0e
mike10911 <mikeymundia@gmail.com>
2023-06-08 16:42:32 -0400

    next/image fix

--- next.config.cjs
+++ next.config.cjs
@@ -4,10 +4,10 @@ const nextConfig = {}
 
 module.exports = nextConfig
 
-// const withImages = Image
-// module.exports = withImages({
-//   inlineImageLimit: false
-// })
+const withImages = Image
+module.exports = withImages({
+  inlineImageLimit: false
+})
 
 module.exports = {
   webpack(config, options) {
@@ -15,7 +15,7 @@ module.exports = {
       test: /\.(ogg|mp3|wav|mpe?g)$/i,
       use: [
         {
-          loader: 'file-loader',
+          loader: 'file',
           options: {
             name: '[name]-[hash].[ext]',
           },
